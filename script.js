3f9f1b36339cf71ec51a721ccdea065467ce6f98
kazel444 <lygatronyt@gmail.com>
2026-07-11 13:15:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,119 @@
-const canvas=document.getElementById("matrix");
+let current=0;
 
-const ctx=canvas.getContext("2d");
+let selected=[];
 
-canvas.width=window.innerWidth;
-
-canvas.height=window.innerHeight;
+let personName="";
 
 function mulaiTes(){
 
-alert("Menu pertanyaan akan dibuat di Part 3.");
+personName=document.getElementById("nama").value;
+
+if(personName==""){
+
+alert("Masukkan nama terlebih dahulu.");
+
+return;
 
 }
 
-window.addEventListener("resize",()=>{
+document.querySelector(".container").style.display="none";
 
-canvas.width=window.innerWidth;
+document.getElementById("quizPage").style.display="flex";
 
-canvas.height=window.innerHeight;
+renderQuestion();
 
-});
+}
+
+function renderQuestion(){
+
+let q=questions[current];
+
+document.getElementById("questionNumber").innerHTML=
+
+"Soal "+(current+1)+" / "+questions.length;
+
+document.getElementById("questionText").innerHTML=q.question;
+
+document.getElementById("progress").style.width=
+
+((current+1)/questions.length*100)+"%";
+
+let html="";
+
+const pilihan=[
+
+"Tidak Pernah",
+
+"Jarang",
+
+"Kadang",
+
+"Sering",
+
+"Selalu"
+
+];
+
+for(let i=1;i<=5;i++){
+
+html+=`
+
+<div class="answer ${selected[current]==i?"selected":""}"
+
+onclick="choose(${i})">
+
+${pilihan[i-1]}
+
+</div>
+
+`;
+
+}
+
+document.getElementById("answers").innerHTML=html;
+
+}
+
+function choose(v){
+
+selected[current]=v;
+
+renderQuestion();
+
+}
+
+function nextQuestion(){
+
+if(selected[current]==null){
+
+alert("Pilih jawaban dahulu.");
+
+return;
+
+}
+
+if(current<questions.length-1){
+
+current++;
+
+renderQuestion();
+
+}else{
+
+alert("Halaman hasil akan dibuat pada Part 4.");
+
+}
+
+}
+
+function prevQuestion(){
+
+if(current>0){
+
+current--;
+
+renderQuestion();
+
+}
+
+}
